c4eb5dc9de6bc754e0643c13b9e87c0b4ef968e6
idevtier <idevtier@gmail.com>
2022-06-09 15:48:32 +0300

    Adding domain package

---
+++ api/domain/dtos.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from typing import Optional
+from datetime import datetime
+
+
+@dataclass
+class Client:
+    telegram_id: int
+    telegram_username: Optional[str]
+    telegram_fullname: str
+    created_at: datetime
+
