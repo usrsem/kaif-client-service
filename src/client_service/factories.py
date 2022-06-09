a80b5602a87b5ec6dca28c0bba6cf8b8f4887855
idevtier <idevtier@gmail.com>
2022-06-09 23:24:56 +0300

    Fixing factories

--- src/client_service/factories.py
+++ src/client_service/factories.py
@@ -1,10 +1,11 @@
 from client_service.loader import DEFAULT_SESSION_FACTORY
 from client_service.repository.repository_uow import RepositoryUow, SqlAlchemyRepositoryUow
 from client_service.web.client_service import V1ClientService
+from config import ASYNC_PG_URL
 
 
 def get_repository_uow() -> RepositoryUow:
-    return SqlAlchemyRepositoryUow(DEFAULT_SESSION_FACTORY())
+    return SqlAlchemyRepositoryUow(ASYNC_PG_URL)
 
 
 def get_client_service() -> V1ClientService:
