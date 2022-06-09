2f01f73e3d54fa93db9d3a791ae042a6f8a98afa
idevtier <idevtier@gmail.com>
2022-06-09 23:24:32 +0300

    Refactoring repository UOW

--- src/client_service/repository/repository_uow.py
+++ src/client_service/repository/repository_uow.py
@@ -1,6 +1,8 @@
 from typing import Protocol
+from sqlalchemy.ext.asyncio.engine import create_async_engine
 
 from sqlalchemy.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm.session import sessionmaker
 
 from client_service.repository.client_repository import ClientRepository, SqlAclhemyClientRepository
 
@@ -15,15 +17,22 @@ class RepositoryUow(Protocol):
 
 
 class SqlAlchemyRepositoryUow:
-    def __init__(self, session: AsyncSession) -> None:
-        self._session: AsyncSession = session
+    def __init__(self, pg_url: str) -> None:
+        self._pg_url = pg_url
         self.clients: ClientRepository
 
     async def __aenter__(self) -> None:
+        self.engine = create_async_engine(self._pg_url)
+
+        async_session = sessionmaker(
+                self.engine, expire_on_commit=False, class_=AsyncSession)
+
+        self._session = async_session()
         self.clients = SqlAclhemyClientRepository(self._session)
 
     async def __aexit__(self, *args) -> None:
         await self.rollback()
+        await self.engine.dispose()
 
     async def commit(self) -> None:
         await self._session.commit()
