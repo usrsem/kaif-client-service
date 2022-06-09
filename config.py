6203c87dc43c30b9a84fca430d2b02d11637cfae
idevtier <idevtier@gmail.com>
2022-06-09 15:48:06 +0300

    Adding config module

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+from pydantic import BaseSettings, Field
+
+prefix: str = "KAIF_CLIENT_SERVICE_"
+
+class Settings(BaseSettings):
+    pg_host: str = os.getenv(prefix + "PG_HOST", "localhost:49153")
+    pg_user: str = os.getenv(prefix + "PG_USER", "postgres")
+    pg_password: str = os.getenv(prefix + "PG_PASSWORD", "postgrespw")
+    pg_db_name: str = os.getenv(prefix + "PG_DB_NAME", "client")
+
+
+settings = Settings()
+
+
+SYNC_PG_URL = (
+    f"postgresql://{settings.pg_user}:{settings.pg_password}"
+    f"@{settings.pg_host}/{settings.pg_db_name}")
+
+ASYNC_PG_URL = (
+    f"postgresql+asyncpg://{settings.pg_user}:{settings.pg_password}"
+    f"@{settings.pg_host}/{settings.pg_db_name}")
+
