b7025775a72ed805a0d56317040189974ffe69ab
idevtier <idevtier@gmail.com>
2022-06-09 23:25:55 +0300

    Adding async pytest

--- src/client_service/loader.py
+++ src/client_service/loader.py
@@ -1,3 +1,4 @@
+import sys
 from sqlalchemy.ext.asyncio.engine import create_async_engine
 from sqlalchemy.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm.session import sessionmaker
@@ -8,9 +9,20 @@ from loguru import logger
 
 log = logger
 
+
 log.info("Loader started")
 
 start_mappers()
+
+async def get_session():
+    engine = create_async_engine(ASYNC_PG_URL, echo=True)
+    async_session = sessionmaker(
+            engine, expire_on_commit=False, class_=AsyncSession)
+
+    async with async_session() as session:
+        yield session
+
+    await engine.dispose()
 
 DEFAULT_SESSION_FACTORY = sessionmaker(
     create_async_engine(ASYNC_PG_URL, echo=True),
